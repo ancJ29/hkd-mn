4cb97e2132c8dc6e740e28c136d7e08f97f96ae4
An <140888823+ancJ29@users.noreply.github.com>
2023-12-10 11:51:22 +0700

    sync@20231210.1151

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import MobileOnlyWarning from "@/components/mobile-only-warning";
 import { theme } from "@/configs/mantine-theme";
 import routes from "@/router";
 import { MantineProvider } from "@mantine/core";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { MobileView } from "react-device-detect";
 import { useRoutes } from "react-router-dom";
 
@@ -10,6 +11,7 @@ const App = () => {
     <MantineProvider theme={theme}>
       <MobileOnlyWarning />
       <MobileView>{useRoutes(routes)}</MobileView>
+      <SpeedInsights />
     </MantineProvider>
   );
 };
